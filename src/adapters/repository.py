e6a5d924ceea2f528fef5a5fd2664125ff7eba21
sae <sae@tega.ru>
2024-11-12 16:18:19 +0300

    repository

--- src/adapters/repository.py
+++ src/adapters/repository.py
@@ -1,13 +1,44 @@
 import abc
 
-from src.domain.ticket import Client
+from src.domain.ticket import User, Ticket
 
 
-class AbstractRepository(abc.ABC):
+class AbstractRepositoryUser(abc.ABC):
+    def __init__(self):
+        self.seen_users: dict[int, User] = {}
+
+    def save(self, user: User):
+        user = self._save(user=user)
+        if user.user_id:
+            self.seen_users[user.user_id] = user
+
+    def get(self, user_id: int) -> User:
+        user = self._get(user_id=user_id)
+        if user.user_id:
+            self.seen_users[user_id] = user
+        return user
+
     @abc.abstractmethod
-    def save(self, client: Client):
+    def _save(self, user: User) -> User:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, client_id) -> Client:
+    def _get(self, user_id: int) -> User:
+        raise NotImplementedError
+
+
+class AbstractRepositoryTicket(abc.ABC):
+    def __init__(self):
+        self.seen_ticketes: dict[int, Ticket] = {}
+
+    @abc.abstractmethod
+    def save(self, ticket: Ticket):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, ticket_id: Ticket) -> Ticket:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_all(self, user_id: int) -> list[Ticket]:
         raise NotImplementedError
--- src/adapters/repository_sqlite.py
+++ src/adapters/repository_sqlite.py
@@ -1,15 +1,37 @@
-from src.adapters.repository import AbstractRepository
-from src.domain.ticket import Client
+import sqlite3
+
+from src.adapters.repository import AbstractRepositoryUser
+from src.domain.ticket import Client, User, Ticket
 
 
-class SQLiteRepository(AbstractRepository):
-    def __init__(self,connect):
+class SQLiteRepositoryUser(AbstractRepositoryUser):
+    def __init__(self, connect:sqlite3.Connection):
+        super().__init__()
         self.connect=connect
 
-    def save(self, client: Client):
-        cur = self.connect.cursor()
 
+    def _save_tickets(self,tickets:list[Ticket],cur:sqlite3.Cursor):
+        insert="INSERT INTO tickets "
 
-    def get(self, client_id) -> Client:
-        cur = self.connect.cursor()
+    def _save(self, user: User) -> User:
+        cursor=self.connect.cursor()
+        if not user.user_id:
+            cursor.execute("INSERT INTO users (client_id,name,is_active) VALUES(:client_id,:name:,:is_active)",
+                {'client_id':user.client.client_id, 'name':user.name, 'is_active':user.is_active})
+            user.user_id=cursor.lastrowid
+        else:
+            cursor.execute("UPDATE users SET client_id=:client_id, name=:name, is_active=:is_active "
+                           "WHERE user_id=:user_id",
+                           {'client_id':user.client.client_id,'name':user.name,'is_active':user.is_active,
+                            'user_id':user.user_id})
+        return user
 
+    def _get(self, user_id: int) -> User:
+        cursor=self.connect.cursor()
+        cursor.execute("SELECT u.user_id, u.name, u.is_active,c.client_id,c.name,c.is_active FROM users u "
+                       "LEFT JOIN client c ON u.client_id=c.client_id "
+                       "WHERE u.user_id=:user_id",
+                       {'user_id':user_id})
+        r=cursor.fetchone()
+        client=Client(client_id=r[3],name=r[4],status=r[5])
+        user=User(user_id=r[0],name=r[1],client=client,tickets=[],status=r[2])
